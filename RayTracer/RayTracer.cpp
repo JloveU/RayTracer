062323c0f30cbc738b1f4ddb7f0c931cdaa5d35d
yuqiang <932967248@qq.com>
2016-05-18 21:31:18 +0800

    **修改场景物体位置到坐标原点附近**

--- RayTracer/RayTracer.cpp
+++ RayTracer/RayTracer.cpp
@@ -24,38 +24,34 @@ int main()
     scene.addAxes();
     // 添加地面（无限平面）
 //     scene.addGeometry(new Plane(Vec3f(0.0, -4.0, 0.0), Vec3f(0.0, 1.0, 0.0), Vec3f(0.6, 0.6, 0.6), Vec3f(0.0, 0.0, 0.0), 0.0, 0.0));
-//     const Vec3f _vertices1[4] = {Vec3f(-30.0, -4.0, 20.0), Vec3f(30.0, -4.0, 20.0), Vec3f(30.0, -4.0, -40.0), Vec3f(-30.0, -4.0, -40.0)};
-//     const std::vector<Vec3f> vertices1(_vertices1, _vertices1 + 4);
-//     scene.addGeometry(new Polygon(vertices1, Vec3f(0.4, 0.4, 0.4), Vec3f(0.0, 0.0, 0.0), 0.0, 0.0));
-    Triangle *triangle1 = new Triangle(Vec3f(-30.0, -4.0, 20.0), Vec3f(30.0, -4.0, 20.0), Vec3f(30.0, -4.0, -40.0));
+    Triangle *triangle1 = new Triangle(Vec3f(-30.0, 0.0, 30.0), Vec3f(30.0, -0.0, 30.0), Vec3f(30.0, 0.0, -30.0));
     triangle1->setTextureFileName("./Textures/texture2.png");
     triangle1->setTextureCoordinates(Vec2f(0.0, 0.0), Vec2f(10.0, 0.0), Vec2f(10.0, 10.0));
     scene.addGeometry(triangle1);
-    Triangle *triangle2 = new Triangle(Vec3f(30.0, -4.0, -40.0), Vec3f(-30.0, -4.0, -40.0), Vec3f(-30.0, -4.0, 20.0), Vec3f(0.4, 0.4, 0.4), Vec3f(0.0, 0.0, 0.0), 0.0, 0.0);
+    Triangle *triangle2 = new Triangle(Vec3f(30.0, 0.0, -30.0), Vec3f(-30.0, 0.0, -30.0), Vec3f(-30.0, 0.0, 30.0), Vec3f(0.4, 0.4, 0.4), Vec3f(0.0, 0.0, 0.0), 0.0, 0.0);
     triangle2->setTextureFileName("./Textures/texture2.png");
     triangle2->setTextureCoordinates(Vec2f(10.0, 10.0), Vec2f(0.0, 10.0), Vec2f(0.0, 0.0));
     scene.addGeometry(triangle2);
     // 添加球体
-    scene.addGeometry(new Sphere(Vec3f(0.0, 2.0, -10.0),  4.0, Vec3f(1.0, 0.0, 0.0), Vec3f(0.0, 0.0, 0.0), 0.5, 1.0));
-//     scene.addGeometry(new Sphere(Vec3f(5.0, -1.0, -5.0), 2.0, Vec3f(0.0, 1.0, 0.0), Vec3f(0.0, 0.0, 0.0), 0.0, 1.0));
-    scene.addGeometry(new Sphere(Vec3f(8.0, 1.0, -10.0),  3.0, Vec3f(1.0, 1.0, 1.0), Vec3f(0.0, 0.0, 0.0), 0.0, 1.0));
-    scene.addGeometry(new Sphere(Vec3f(-7, 0.0, -10.0), 3.0, Vec3f(0.0, 1.0, 0.0), Vec3f(0.0, 0.0, 0.0), 0.0, 1.0));
-//     // 添加三角形
-//     scene.addGeometry(new Triangle(Vec3f(-5.0, 2.0, -8.0), Vec3f(5.0, 2.0, -8.0), Vec3f(0.0, 10.66, -8.0), Vec3f(1.0, 1.0, 0.0), Vec3f(0.0, 0.0, 0.0), 0.0, 1.0));
-//     // 添加五边形（多边形）
-//     const Vec3f _vertices1[5] = {Vec3f(-2.0, 0.0, -20.0), Vec3f(2.0, 0.0, -20.0), Vec3f(2.0, 2.0, -18.0), Vec3f(0.0, 4.0, -16.0), Vec3f(-2.0, 2.0, -18.0)};
-//     const std::vector<Vec3f> vertices2(_vertices1, _vertices1 + 5);
-//     scene.addGeometry(new Polygon(vertices2, Vec3f(1.0, 0.0, 1.0), Vec3f(0.0, 0.0, 0.0), 0.0, 1.0));
-//     // 添加长方体
-//     scene.addGeometry(new Box(Vec3f(5.0, 2.0, -10.0), Vec3f(4.0, 2.0, 2.0), Vec3f(0.0, 1.0, 1.0), Vec3f(0.0, 0.0, 0.0), 0.0, 0.2));
-//     // 添加四面体（组合几何图形）
-//     GeometryUnion *geometryUnion1 = new GeometryUnion(Vec3f(0.0, 0.0, 1.0), Vec3f(0.0, 0.0, 0.0), 0.0, 0.2);
-//     float geometryUnion1Size = 5;
-//     geometryUnion1->addGeometry(new Triangle(Vec3f(-8.0, 3.0, -13.0), Vec3f(-8.0 + geometryUnion1Size, 3.0, -13.0), Vec3f(-8.0, 3.0 + geometryUnion1Size, -13.0)));
-//     geometryUnion1->addGeometry(new Triangle(Vec3f(-8.0, 3.0, -13.0), Vec3f(-8.0, 3.0 + geometryUnion1Size, -13.0), Vec3f(-8.0, 3.0, -13.0 + geometryUnion1Size)));
-//     geometryUnion1->addGeometry(new Triangle(Vec3f(-8.0, 3.0, -13.0), Vec3f(-8.0, 3.0, -13.0 + geometryUnion1Size), Vec3f(-8.0 + geometryUnion1Size, 3.0, -13.0)));
-//     geometryUnion1->addGeometry(new Triangle(Vec3f(-8.0 + geometryUnion1Size, 3.0, -13.0), Vec3f(-8.0, 3.0 + geometryUnion1Size, -13.0), Vec3f(-8.0, 3.0, -13.0 + geometryUnion1Size)));
-//     scene.addGeometry(geometryUnion1);
+    scene.addGeometry(new Sphere(Vec3f(0.0, 4.0, 0.0),  4.0, Vec3f(1.0, 0.0, 0.0), Vec3f(0.0, 0.0, 0.0), 0.5, 1.0));
+    scene.addGeometry(new Sphere(Vec3f(8.0, 4.0, 0.0),  3.0, Vec3f(1.0, 1.0, 1.0), Vec3f(0.0, 0.0, 0.0), 0.0, 1.0));
+    scene.addGeometry(new Sphere(Vec3f(-7.0, 4.0, 0.0), 3.0, Vec3f(0.0, 1.0, 0.0), Vec3f(0.0, 0.0, 0.0), 0.0, 1.0));
+    // 添加三角形
+    scene.addGeometry(new Triangle(Vec3f(-3.0, 2.0, -9.0), Vec3f(3.0, 2.0, -9.0), Vec3f(0.0, 7.196, -9.0), Vec3f(1.0, 1.0, 0.0), Vec3f(0.0, 0.0, 0.0), 0.0, 1.0));
+    // 添加五边形（多边形）
+    const Vec3f _vertices1[5] = {Vec3f(-7.0, 2.0, -4.0), Vec3f(-3.0, 2.0, -8.0), Vec3f(-3.0, 6.0, -8.0), Vec3f(-5.0, 10.0, -6.0), Vec3f(-7.0, 6.0, -4.0)};
+    const std::vector<Vec3f> vertices1(_vertices1, _vertices1 + 5);
+    scene.addGeometry(new Polygon(vertices1, Vec3f(1.0, 0.0, 1.0), Vec3f(0.0, 0.0, 0.0), 0.0, 1.0));
+    // 添加长方体
+    scene.addGeometry(new Box(Vec3f(3.0, 2.0, -6.0), Vec3f(4.0, 2.0, 2.0), Vec3f(0.0, 1.0, 1.0), Vec3f(0.0, 0.0, 0.0), 0.0, 0.2));
+    // 添加四面体（组合几何图形）
+    GeometryUnion *geometryUnion1 = new GeometryUnion(Vec3f(0.0, 0.0, 1.0), Vec3f(0.0, 0.0, 0.0), 0.0, 0.2);
+    float geometryUnion1Size = 5;
+    geometryUnion1->addGeometry(new Triangle(Vec3f(-8.0, 3.0, 4.0), Vec3f(-8.0 + geometryUnion1Size, 3.0, 4.0), Vec3f(-8.0, 3.0 + geometryUnion1Size, 4.0)));
+    geometryUnion1->addGeometry(new Triangle(Vec3f(-8.0, 3.0, 4.0), Vec3f(-8.0, 3.0 + geometryUnion1Size, 4.0), Vec3f(-8.0, 3.0, 4.0 + geometryUnion1Size)));
+    geometryUnion1->addGeometry(new Triangle(Vec3f(-8.0, 3.0, 4.0), Vec3f(-8.0, 3.0, 4.0 + geometryUnion1Size), Vec3f(-8.0 + geometryUnion1Size, 3.0, 4.0)));
+    geometryUnion1->addGeometry(new Triangle(Vec3f(-8.0 + geometryUnion1Size, 3.0, 4.0), Vec3f(-8.0, 3.0 + geometryUnion1Size, 4.0), Vec3f(-8.0, 3.0, 4.0 + geometryUnion1Size)));
+    scene.addGeometry(geometryUnion1);
 //     // 添加Mesh
 //     Mesh *mesh1 = new Mesh(Vec3f(0.0, 0.0, 1.0), Vec3f(0.0, 0.0, 0.0), 0.0, 0.2);
 //     mesh1->loadObj("Models/PAC-216/OBJ/PAC-216-wheel.obj");
@@ -65,12 +61,12 @@ int main()
     scene.addGeometry(new Sphere(Vec3f(-60.0, 120.0, -120.0), 30.0, Vec3f(1.0, 1.0, 1.0), Vec3f(1.0, 1.0, 1.0), 0.0, 0.0));
 
     // 相机
-    Camera camera(Vec3f(0.0, 0.0, 10.0), Vec3f(0.0, 0.0, -1.0), Vec3f(0.0, 1.0, 0.0), 30, 1, 640, 480);
+    Camera camera(Vec3f(0.0, 0.0, 40.0), Vec3f(0.0, 0.0, -1.0), Vec3f(0.0, 1.0, 0.0), 30, 1, 640, 480);
 
     // 动画展示（不停地移动相机并且转动相机的朝向）
     // 动画中，场景中的所有几何体以及光源固定不动，只移动相机位置及指向
     // 相机运动规则为：在以(0,0,-20)为球心，40为半径的球体外表面上，xz平面做匀速圆周运动，同时在平行于y轴的各平面上并且在xz平面上方做反复匀速半圆周运动，相机始终指向球心(0,0,-20)，并且相机像面始终与相机指向垂直（由此可确定相机头顶指向）
-    Vec3f cameraMotionSphereCenter(0.0, 0.0, -10.0);
+    Vec3f cameraMotionSphereCenter(0.0, 0.0, 0.0);
 //     Vec3f cameraMotionSphereCenter(1.17518, 0.663388, -0.00610712);  // PAC-216-wheel.obj
     float circleRadius = 40;
 //     float circleRadius = 3;  // PAC-216-wheel.obj
